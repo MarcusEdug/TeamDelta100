8dfdb3a4d46dacc859f2ef0db512d0ab86d5fc54
msved <marcus.svedberg.java23@edu.edugrade.se>
2023-12-07 16:11:23 +0100

    Lagt till persistence-fil

--- src/main/java/com/example/teamdelta100/controller/GameController.java
+++ src/main/java/com/example/teamdelta100/controller/GameController.java
@@ -1,18 +1,20 @@
 package com.example.teamdelta100.controller;
-
 import com.example.teamdelta100.entity.Games;
+import com.example.teamdelta100.entity.Player;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
-import javax.persistence.Persistence;
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class GameController {
     public static final EntityManagerFactory ENTITY_MANAGER_FACTORY = Persistence.createEntityManagerFactory("hibernate");
 
-    // CREATE
+    // Create
     public boolean save(Games games) {
-        // "Boiler plate" of code, recurring code throughout the code
+        // Boiler plate
         EntityManager entityManager = ENTITY_MANAGER_FACTORY.createEntityManager();
         EntityTransaction transaction = null;
         try {
@@ -31,4 +33,78 @@ public class GameController {
         }
         return false;
     }
+
+    //Read
+    public List<Games> getAll(boolean printOut) {
+        EntityManager entityManager = ENTITY_MANAGER_FACTORY.createEntityManager();
+        EntityTransaction transaction = null;
+        try {
+            transaction = entityManager.getTransaction();
+            transaction.begin();
+            List<Games> listToReturn = new ArrayList<>(entityManager.createQuery("FROM Games", Games.class).getResultList());
+            transaction.commit();
+            if (printOut) {
+                for (Games games :
+                        listToReturn) {
+                    System.out.println(games.getGamesId() + ". " + games.getGameName());
+                    for (Player player :
+                            player.getPlayerName()) {
+                        System.out.println(games.getGameName());
+                    }
+                }
+            }
+            return listToReturn;
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            e.printStackTrace();
+        } finally {
+            entityManager.close();
+        }
+        return null;
+    }
+
+    //Update
+    public boolean updateGames(Games games) {
+        EntityManager entityManager = ENTITY_MANAGER_FACTORY.createEntityManager();
+        EntityTransaction transaction = null;
+        try {
+            transaction = entityManager.getTransaction();
+            transaction.begin();
+            entityManager.merge(games);
+            transaction.commit();
+            return true;
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            e.getMessage();
+        }
+        return false;
+    }
+
+    //Delete
+    public boolean deleteGames(Games games) {
+        EntityManager entityManager = ENTITY_MANAGER_FACTORY.createEntityManager();
+        EntityTransaction transaction = null;
+        try {
+            transaction = entityManager.getTransaction();
+            transaction.begin();
+            //Om entitet finns med så ta bort spel, annars slå ihop och sen ta bort
+            entityManager.remove(entityManager.contains(games) ? games:entityManager.merge(games));
+            transaction.commit();
+            return true;
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            e.getMessage();
+        } finally {
+                entityManager.close();
+        }
+        return false;
+
+    }
+
 }
--- src/main/java/com/example/teamdelta100/entity/Games.java
+++ src/main/java/com/example/teamdelta100/entity/Games.java
@@ -1,4 +1,81 @@
 package com.example.teamdelta100.entity;
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
+
+//CRUD-operationer
+@Entity
+@Table (name = "games")
 public class Games {
+    //Primary key
+    @Id
+    //Id genereras av databasen
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "games_id")
+    private int gamesId;
+    @Column (length = 50)
+    private String gameName;
+
+    @Column (length = 50)
+    private String gameGenre;
+
+    @Column (name = "player_id")
+    private int playerId;
+
+    //OneToMany - Ett spel till många spelare/lag
+    //Fetch - fråga alltid efter entiteter och children.
+    //Cascade - sparar entitet och children på en gång
+    //Mapped - Förhindrar den här sidan att skapa en extra tabell för relational mapping
+    //Mapped - säger till hibernate att den motsatta sidan har kontroll, inget behov av att skapa tabeller (behövs den här?)
+    @OneToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL, mappedBy = "player" + "teams")
+    private List<Games> gamesToChose = new ArrayList<>();
+
+    @JoinColumn (name = "player_id")
+    @JoinColumn (name = "team_id")
+    private Player player;
+    private Teams teams;
+
+    public Games() {
+    }
+
+    public String getGameName() {
+        return gameName;
+    }
+
+    public void setGameName(String gameName) {
+        this.gameName = gameName;
+    }
+
+    public Player getPlayer() {
+        return player;
+    }
+
+    public void setPlayer(Player player) {
+        this.player = player;
+    }
+
+    public Teams getTeams() {
+        return teams;
+    }
+
+    public void setTeams(Teams teams) {
+        this.teams = teams;
+    }
+
+    public void setGamesId(int id) {
+        this.gamesId = id;
+    }
+
+    public String getGameGenre() {
+        return gameGenre;
+    }
+
+    public void setGameGenre(String gameGenre) {
+        this.gameGenre = gameGenre;
+    }
+
+    public int getGamesId() {
+        return gamesId;
+    }
 }
