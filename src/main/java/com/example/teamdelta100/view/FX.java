247e80844cb767f0b80d16104e36fa37c804c9d6
Rosen <Alexander.rosen.JAVA23@edu.edugrade.se>
2023-12-11 15:06:33 +0100

    4 2023-12-11

--- src/main/java/com/example/teamdelta100/view/FX.java
+++ src/main/java/com/example/teamdelta100/view/FX.java
@@ -6,6 +6,8 @@ import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.control.cell.TextFieldListCell;
+import javafx.scene.control.cell.TextFieldTableCell;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
@@ -17,7 +19,7 @@ import java.util.List;
 import java.util.Scanner;
 
 
-public class FX extends Application implements Share {
+public class FX extends Application {
     TeamsController controller = new TeamsController();
     Popup popup = new Popup();
     TableView tableView;
@@ -31,16 +33,15 @@ public class FX extends Application implements Share {
         Button add = button("Add team");
         Button assign = button("Assign Player");
         Button delete = button("Delete team");
-        Button update = button("Update team");
         Button logOut = button("Log out");
 
-        TableView viewTeams = table();
+        tableView = table();
 
         VBox buttonV = new VBox(10);
-        buttonV.getChildren().addAll(add,assign,delete,update,logOut);
+        buttonV.getChildren().addAll(add,assign,delete,logOut);
 
         AnchorPane anchorPane = new AnchorPane();
-        anchorPane.getChildren().addAll(viewTeams, buttonV);
+        anchorPane.getChildren().addAll(tableView, buttonV);
         AnchorPane.setTopAnchor(buttonV,100.0);
         AnchorPane.setLeftAnchor(buttonV,270.0);
        // AnchorPane.setRightAnchor(buttonV,50.0);
@@ -84,24 +85,6 @@ public class FX extends Application implements Share {
 
 
             }
-            else if (input.equals("Update team")) {
-
-
-
-
-                /*controller.getAll(true);
-                System.out.print("Välj id:");
-                Teams teamsToUpdate = controller.getTeamsById(new Scanner(System.in).nextInt());
-                System.out.print("Ändra namn från " + teamsToUpdate.getName() + " till?: ");
-                teamsToUpdate.setName(new Scanner(System.in).nextLine());
-                if(controller.updateTeams(teamsToUpdate)){
-                    System.out.println("Teams updated");
-                } else {
-                    System.out.println("Teams update failed");
-                };
-
-                 */
-            }
 
             else if (input.equals("Log out")) {
                 window.close();
@@ -114,11 +97,15 @@ public class FX extends Application implements Share {
 
     public TableView table (){
         tableView = new TableView<Teams>();
+        tableView.setEditable(true);
+
         TableColumn teamIdColumn = new TableColumn<Teams, Integer>("Team ID");
         teamIdColumn.setCellValueFactory(new PropertyValueFactory<Teams, Integer>("id"));
 
+
         TableColumn teamNameColumn = new TableColumn<Teams, String>("Team Name");
         teamNameColumn.setCellValueFactory(new PropertyValueFactory<Teams, String>("name"));
+        teamNameColumn.setCellFactory(TextFieldTableCell.forTableColumn());
 
         tableView.getColumns().addAll(teamIdColumn,teamNameColumn);
 
@@ -129,6 +116,7 @@ public class FX extends Application implements Share {
         for (Teams temp : controller.tableUpdate(true) ) {
             tableView.getItems().add(temp);
         }
+
 
     }
 
